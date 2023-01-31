d98e74d7f2c4e722610bba516fcfecd16aa3b949
Matthew C <cnymhw86@gmail.com>
2023-01-30 22:06:05 -0500

    added new tag within tag routes

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -36,8 +36,14 @@ res.status(500).json(err);
 
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Location.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
