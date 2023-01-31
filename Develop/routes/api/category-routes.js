2b7617ecf613160b781ebeef6242ed33bf388899
Matthew C <cnymhw86@gmail.com>
2023-01-30 21:43:15 -0500

    added router.get for all categories

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,26 +3,38 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// find all categories
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      // be sure to include its associated Products
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  
+
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  
 });
 
 module.exports = router;
